457d3242a9ae76c715b51738087d494f6d65d514
getdat <getdat@getdat.getd.at>
2016-12-18 09:18:08 +0100

    add the initial theme (by Shadowfied)

---
+++ www/index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="assets/style.css"/>
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=2.0, user-scalable=no" />
+</head>
+
+
+<body>
+<div class="header">
+	<div class="logo">getdat.</div>
+	<div class="social">
+	<a class="fa fa-facebook-square sociallink"></a>
+	<a class="fa fa-twitter-square sociallink"></a>
+	</div>
+</div>
+
+<div class="wrapper">
+	<div class="main">
+	<div class="heading">
+	Welcome to getd.at!
+	</div>
+
+	<div class="upload">
+	Drag and drop your file here to upload instantly!
+	</div>
+
+		<div class="features">
+		<div class="feature">
+			<div class="featureicon"><i class="fa fa-file" aria-hidden="true"></i></div>
+			<h3>Upload your files!</h3>
+			<p>
+				Upload, share and hotlink any file from our fast servers!
+			</p>
+		</div>
+
+		<div class="feature">
+			<div class="featureicon"><i class="fa fa-heart" aria-hidden="true"></i></div>
+			<h3>Kawaii!!</h3>
+			<p>
+			Enjoy the kawaiiest anime cuties while uploading your files!
+			</p>
+		</div>
+
+		<div class="feature">
+			<div class="featureicon"><i class="fa fa-rocket" aria-hidden="true"></i></div>
+			<h3>Super fast speeds!</h3>
+			<p>
+			Enjoy high speed downloads from and uploads to our gigabit servers!
+			</p>
+		</div>
+		<div class="azunyan"></div>
+		<div class="nepgear"></div>
+		</div>
+
+	</div>
+
+	<div class="footer">
+	© 2016 getd.at
+	</div>
+
+</div>
+
+</body>
+</html>
